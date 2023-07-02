480a52c0984d501a6e1a0c3ea2a98eb66e9b68f7
adeliamara <adeliamara13@gmail.com>
2023-07-02 09:57:52 -0300

    feat: documentacao da api com swagger

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,10 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config'
 import * as cors from 'cors';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
+
+   // Configuração do Swagger
+   const config = new DocumentBuilder()
+   .setTitle('API livrou')
+   .setDescription('Api da Plataforma Livrou')
+   .setVersion('1.0')
+   .build();
+
+ const document = SwaggerModule.createDocument(app, config);
+ SwaggerModule.setup('api', app, document);
 
   app.use(cors())
   await app.listen(3000);
